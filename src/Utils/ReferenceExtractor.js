fcab74784e1f34b39532d9a0491b24fd5883e54d
mosasiru <mosaafi@gmail.com>
2016-11-16 14:11:06 +0900

    fix issue 786: enable reference in embedded_list

--- src/Utils/ReferenceExtractor.js
+++ src/Utils/ReferenceExtractor.js
@@ -4,7 +4,13 @@ export default {
         return this.indexByName(fields.filter(f => f.type() === 'referenced_list'));
     },
     getReferences(fields, withRemoteComplete, optimized = null) {
-        let references = fields.filter(f => f.type() === 'reference' || f.type() === 'reference_many');
+        var references = fields.filter(f => f.type() === 'reference' || f.type() === 'reference_many');
+        let embed = fields.filter(f => f.type() === 'embedded_list')
+        for (let i = 0, l = embed.length ; i < l ; i++) {
+            let field = embed[i];
+            references = references.concat(field.targetFields().filter(f => f.type() === 'reference' || f.type() === 'reference_many') );
+        }
+
         if (withRemoteComplete === true) {
             references = references.filter(r => r.remoteComplete());
         } else if (withRemoteComplete === false) {
